d522c916f6e856d47e375618c1b0784c7406e842
Sayantan Guha <sayantanguha@hotmail.com>
2016-05-08 01:24:35 -0700

    7. Reverse Integer

---
+++ LeetCode/src/Easy/ReverseInteger.java
@@ -0,0 +1,33 @@
+package Easy;
+
+public class ReverseInteger {
+
+	public static void main(String[] args) {
+		System.out.println("The reverse of the integer " + reverseint(-1234));
+
+	}
+	
+	public static int reverseint(int x)
+	{
+		boolean flag=false;
+		int num =x;
+		int reverse =0;
+		if(x<0)
+		{
+			flag = true;
+			num = 0 - num;
+		}
+		while(num!=0)
+		{
+			int mod = num % 10;
+			num /= 10;
+			reverse = reverse * 10 + mod;
+		}
+		if(flag)
+		{
+			reverse = 0-reverse;
+		}
+		return reverse;
+	}
+
+}
